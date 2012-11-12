14fc3e877ca819645764b4770081e8c61bd631dd
Magyar Dávid <magyardavid@gmail.com>
2012-11-12 22:45:45 +0100

    regexp keresés
    a keresőboxba írt szöveg automatikusan .*szöveg.* regexpként fut le az
    eredmények egyelőre rendezetlenek

--- gui.py
+++ gui.py
@@ -1,10 +1,13 @@
 import easygui as eg
+import re as regexp
 
 #fajlbeolvasas es inicalizalas
 lines = tuple(open("page_rank_output.txt", 'r'))
 pagenumber = int(lines[0])
 pages=[]
 pageranks=[]
+eredmenyek=[]
+valasz = []
 #oldalak beolvasasa
 for i in range(1,pagenumber+1):
 	actline = lines[i].split()
@@ -12,9 +15,24 @@ for i in range(1,pagenumber+1):
 	pageranks.insert(int(actline[0]),actline[2])
 
 reply = eg.enterbox(msg='Kereses')
+
+#regexp kereses
+pattern= regexp.compile('.*%s.*'%reply)
+for url in pages:
+	matchobj = pattern.search(url)
+	if matchobj:
+		eredmenyek.append(matchobj.group())
+
+
+for e in eredmenyek:
+	i = pages.index(e)
+	valasz.append("{} {} {} \n".format(i+1,pages[i],pageranks[i]))
+eg.textbox(text=valasz)
+'''
 try:
 	i = pages.index(reply)
 	eredmeny="{} {} {}\n".format(i+1,pages[i],pageranks[i])
 except ValueError:
 	eredmeny="Nem talalhato"
 eg.textbox(text=eredmeny)
+'''
