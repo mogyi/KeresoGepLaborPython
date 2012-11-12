dc4479e7167dbc94ca6ddc8a0e6f432017910e94
Magyar Dávid <magyardavid@gmail.com>
2012-11-12 19:24:50 +0100

    beolvasások
    oldalak, gráfok beolvasása, pagerank kezdet, végre

--- kereso.py
+++ kereso.py
@@ -1,22 +1,29 @@
-import numpy #tobbdimenzios tombok miatt
 
-#fajlbeolvasas
+#fajlbeolvasas es inicalizalas
 lines = tuple(open("top_depth.txt", 'r'))
 pagenumber = int(lines[0])
 pages = []
-csucsokbol =[list for i in range(pagenumber)]
-csucsokba = [list for i in range(pagenumber)]
+csucsokbol =[[] for i in range(0,pagenumber+1)]
+csucsokba =[[] for i in range(0,pagenumber+1)]
 kifok = [0 for i in range(pagenumber)]
-print kifok[200]
 
+#oldalak beolvasasa
 for i in range(1,pagenumber+1):
 	actline = lines[i].split()
 	pages.insert(int(actline[0]),actline[1])
 
 linknumber = int(lines[pagenumber+1])
 
-
-for i in range(0,linknumber):
+#gráf beolvasasa
+for i in range(pagenumber+2,linknumber-1):
 	act = lines[i].split()
-	csucsokbol.insert(int(act[0]),int(act[1]))
-	csucsokba.insert(int(act[1]),int(act[0]))
+	forras = int(act[0])
+	cel = int(act[1])
+	csucsokbol[forras].append(cel)
+	csucsokba[cel].append(forras)
+	kifok[forras]=kifok[forras]+1
+
+
+#pagerank kezdeti	
+pagerank = [1/pagenumber for i in range(0,pagenumber+1)]
+
